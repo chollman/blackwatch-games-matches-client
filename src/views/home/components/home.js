13b1d754c77e9c82f84b1ae0ad05dce48d1e157b
Claudio Hollman <hollman@strands.com>
2020-06-20 20:42:33 -0300

    Prueba home Pampi

--- src/views/home/components/home.js
+++ src/views/home/components/home.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Segment, Header, Icon, Image, Grid } from 'semantic-ui-react';
+import { Segment, Image, Grid, Icon } from 'semantic-ui-react';
 import src from '../../../styles/img/pampi.png';
 
 const Home = () => {
     return (
-        <Grid container stackable verticalAlign="middle">
+        <Grid container stackable>
+            <Grid.Row>
+                <Grid.Column width={8}>
+                    <Segment className="content-box" textAlign="center">
+                        <Segment basic>
+                            <Image fluid src={src} centered />
+                        </Segment>
+                    </Segment>
+                </Grid.Column>
+                <Grid.Column width={8} stretched>
+                    <Segment
+                        className="content-box"
+                        textAlign="center"
+                        placeholder
+                    >
+                        Pampita the Basset
+                    </Segment>
+                </Grid.Column>
+            </Grid.Row>
             <Grid.Row>
                 <Grid.Column width={16}>
                     <Segment className="content-box" textAlign="center">
-                        <Header icon>
-                            <Icon name="pdf file outline" />
-                            Home
-                            <Segment vertical>
-                                <Image fluid src={src} centered />
-                            </Segment>
-                        </Header>
+                        Visitenla en
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.instagram.com/pampibasset/?hl=es-la"
+                        >
+                            <Icon size="large" name="instagram" />
+                            @pampibasset
+                        </a>
                     </Segment>
                 </Grid.Column>
             </Grid.Row>
